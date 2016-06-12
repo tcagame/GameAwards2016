f22f0958a3636d5731912b2e5f643e072ea26d90
Masato <masato12021996@gmail.com>
2016-06-12 11:37:43 +0900

    分割マップの描画

--- Solution/MapMaker/MapMaker.cpp
+++ Solution/MapMaker/MapMaker.cpp
@@ -11,6 +11,10 @@ const std::pair< int, int > DIR[ 4 ] = {
 
 MapMaker::MapMaker( ) {
 	_ground_maker = GroundMakerPtr( new GroundMaker( ) );
+	putFileName( );
+	load( );
+	mapMake( );
+	save( );
 }
 
 MapMaker::~MapMaker( ) {
@@ -27,76 +31,92 @@ void MapMaker::load( ){
 }
 
 void MapMaker::mapMake( ) {
-	makePlane( );
-	makeDesert( );
-	makeMountain( );
-	makeRiver( );
+	for ( int i = 0; i < _map_height; i++ ) {
+		for ( int j = 0; j < _map_width; j++ ) {
+			makePlane( j, i );
+			makeDesert( j, i );
+			makeMountain( j, i );
+			makeRiver( j, i );
+		}
+	}
 }
 
 void MapMaker::save( ) {
 	_ground_maker->save( );
 }
 
-void MapMaker::makeMountain( ) {
-	for ( int i = 0; i < _map_height; i++ ) {
-		for ( int j = 0; j < _map_width; j++ ) {
-			int num = 0;
-			for ( int k = 0; k < 4; k++ ) {
-				num = num << 1;
-				GROUND_CHIP_TYPE chip = _ground_maker->getGroundChip( j + DIR[ k ].first, i + DIR[ k ].second );
-				if ( chip == GROUND_CHIP_TYPE_MOUNTAIN ) {
-					num += 1;
-				}
-			}
-			_mountain_map.push_back( num );
+void MapMaker::makeMountain( int mx, int my ) {
+	int num = 0;
+	for ( int i = 0; i < 4; i++ ) {
+		num = num << 1;
+		GROUND_CHIP_TYPE chip = _ground_maker->getGroundChip( mx + DIR[ i ].first, my + DIR[ i ].second );
+		if ( chip == GROUND_CHIP_TYPE_MOUNTAIN ) {
+			num += 1;
 		}
 	}
+	_mountain_map.push_back( num );
 }
 
-void MapMaker::makeRiver( ) {
-	for ( int i = 0; i < _map_height; i++ ) {
-		for ( int j = 0; j < _map_width; j++ ) {
-			int num = 0;
-			for ( int k = 0; k < 4; k++ ) {
-				num = num << 1;
-				GROUND_CHIP_TYPE chip = _ground_maker->getGroundChip( j + DIR[ k ].first, i + DIR[ k ].second );
-				if ( chip == GROUND_CHIP_TYPE_RIVER ) {
-					num += 1;
-				}
-			}
-			_river_map.push_back( num );
+void MapMaker::makeRiver( int mx, int my ) {
+	int num = 0;
+	for ( int i = 0; i < 4; i++ ) {
+		num = num << 1;
+		GROUND_CHIP_TYPE chip = _ground_maker->getGroundChip( mx + DIR[ i ].first, my + DIR[ i ].second );
+		if ( chip == GROUND_CHIP_TYPE_RIVER ) {
+			num += 1;
 		}
 	}
+	_river_map.push_back( num );
 }
 
-void MapMaker::makePlane( ) {
-	for ( int i = 0; i < _map_height; i++ ) {
-		for ( int j = 0; j < _map_width; j++ ) {
-			int num = 0;
-			for ( int k = 0; k < 4; k++ ) {
-				num = num << 1;
-				GROUND_CHIP_TYPE chip = _ground_maker->getGroundChip( j + DIR[ k ].first, i + DIR[ k ].second );
-				if ( chip == GROUND_CHIP_TYPE_PLAIN ) {
-					num += 1;
-				}
-			}
-			_plane_map.push_back( num );
+void MapMaker::makePlane( int mx, int my ) {
+	int num = 0;
+	for ( int i = 0; i < 4; i++ ) {
+		num = num << 1;
+		GROUND_CHIP_TYPE chip = _ground_maker->getGroundChip( mx + DIR[ i ].first, my + DIR[ i ].second );
+		if ( chip == GROUND_CHIP_TYPE_PLAIN ) {
+			num += 1;
 		}
 	}
+	_plane_map.push_back( num );
 }
 
-void MapMaker::makeDesert( ) {
-	for ( int i = 0; i < _map_height; i++ ) {
-		for ( int j = 0; j < _map_width; j++ ) {
-			int num = 0;
-			for ( int k = 0; k < 4; k++ ) {
-				num = num << 1;
-				GROUND_CHIP_TYPE chip = _ground_maker->getGroundChip( j + DIR[ k ].first, i + DIR[ k ].second );
-				if ( chip == GROUND_CHIP_TYPE_DESERT ) {
-					num += 1;
-				}
-			}
-			_desert_map.push_back( num );
+void MapMaker::makeDesert( int mx, int my ) {
+	int num = 0;
+	for ( int i = 0; i < 4; i++ ) {
+		num = num << 1;
+		GROUND_CHIP_TYPE chip = _ground_maker->getGroundChip( mx + DIR[ i ].first, my + DIR[ i ].second );
+		if ( chip == GROUND_CHIP_TYPE_DESERT ) {
+			num += 1;
 		}
 	}
+	_desert_map.push_back( num );
+}
+
+int MapMaker::getWidth( ) const {
+	return _map_width;
+}
+
+int MapMaker::getHeight( ) const {
+	return _map_height;
+}
+
+MAP_TYPE MapMaker::getMapType( int mx, int my, GROUND_CHIP_TYPE type ) const {
+	MAP_TYPE chip;
+	int idx = my * _map_width + mx;
+	switch ( type ) {
+		case GROUND_CHIP_TYPE_PLAIN:
+			chip = _plane_map[ idx ];
+			break;
+		case GROUND_CHIP_TYPE_DESERT:
+			chip = _desert_map[ idx ];
+			break;
+		case GROUND_CHIP_TYPE_MOUNTAIN:
+			chip = _mountain_map[ idx ];
+			break;
+		case GROUND_CHIP_TYPE_RIVER:
+			chip = _river_map[ idx ];
+			break;
+	}
+	return chip;
 }
--- Solution/MapMaker/main.cpp
+++ Solution/MapMaker/main.cpp
@@ -1,5 +1,15 @@
+#include "Framework.h"
 #include "MapMaker.h"
+#include "Drawer.h"
+#include "Viewer.h"
 
 int main( ) {
+	FrameworkPtr fw = Framework::getInstance( );
+	DrawerPtr   drawer   = DrawerPtr  ( new Drawer( "Resource2D" )   );
 	MapMakerPtr map_maker = MapMakerPtr( new MapMaker( ) );
+	ViewerPtr viewer = ViewerPtr( new Viewer( map_maker ) );
+
+
+	fw->addTask( Drawer::getTag( ), drawer );
+	fw->addTask( Viewer::getTag( ), viewer );
 }
