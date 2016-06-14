e4705c05d093b54c034eb1dd7adb25488cf14b0a
Masato <Masato12021996@gmail.com>
2016-06-14 12:17:12 +0900

    modelmakerアプリケーション化

--- Solution/ModelMaker/main.cpp
+++ Solution/ModelMaker/main.cpp
@@ -1,25 +1,11 @@
 #include "Framework.h"
-#include "MapMaker.h"
-#include "Drawer.h"
-#include "Viewer.h"
-#include "ModelManager.h"
-#include "FileManager.h"
-#include "Model.h"
-#include "Camera.h"
+#include "ModelMaker.h"
 
 int main( ) {
 	FrameworkPtr fw = Framework::getInstance( );
-	DrawerPtr   drawer   = DrawerPtr  ( new Drawer( "Resource2D" )   );
-	FileManagerPtr file_manager = FileManagerPtr( new FileManager(  ) );
-	ModelPtr model = ModelPtr( new Model(  ) );
-	CameraPtr camera = CameraPtr( new Camera( ) );
-	MapMakerPtr map_maker = MapMakerPtr( new MapMaker(  ) );
-	ModelManagerPtr model_manager = ModelManagerPtr( new ModelManager( map_maker, file_manager, model ) );
-	
-	ViewerPtr viewer = ViewerPtr( new Viewer( map_maker, file_manager, model ) );
+	ModelMakerPtr model_maker = ModelMakerPtr( new ModelMaker( ) );
 
-	fw->addTask( Drawer::getTag( ), drawer );
-	fw->addTask( Viewer::getTag( ), viewer );
-	fw->addTask( Camera::getTag( ), camera );
+
+	fw->addTask( ModelMaker::getTag( ), model_maker );
 
 }
