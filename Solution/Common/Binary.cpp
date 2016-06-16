920bd8bc0313ec90d2e7d8df62da4b4bddfc2bd7
おくてぃ～ <okuty.phone@gmail.com>
2016-06-16 14:45:33 +0900

    Commonプロジェクト作成

---
+++ Solution/Common/Binary.cpp
@@ -0,0 +1,11 @@
+#include "Binary.h"
+
+
+Binary::Binary()
+{
+}
+
+
+Binary::~Binary()
+{
+}
---
+++ Solution/Common/Binary.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "smart_ptr.h"
+
+PTR( Binary );
+
+class Binary {
+public:
+	Binary();
+	virtual ~Binary();
+};
+
